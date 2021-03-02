c044e2bb271ed73bcbb8a7a50f5238f2821b67ce
Tom Schofield <tomschofieldart@gmail.com>
2021-03-02 11:54:50 +0000

    major marker changes

--- src/app/get-elections.service.ts
+++ src/app/get-elections.service.ts
@@ -8,9 +8,11 @@ export class GetElectionsService {
 	constructor(private http: HttpClient) { }
 	//
 	//dataUrl='http://192.168.0.51:8888/ECCPEC_code/PHP/getElections.php';
-	dataUrl='http://localhost:9999/ECCPEC_code/PHP/getElections.php';
+	//dataUrl='http://localhost:9999/ECCPEC_code/PHP/getElections.php';
 	//	dataUrl='/ECPPEC/php/getElections.php';
-	//dataUrl='https://ecppec.ncl.ac.uk/php/getElections.php';
+	//dataUrl='https://ecppec.ncl.ac.uk/php/getElections.php'
+	dataUrl='https://ecppec.ncl.ac.uk/php/getElections.php';
+	//dataUrl='/php/getElections.php';
 	getData() {
 		return this.http.get(this.dataUrl);
 	}
