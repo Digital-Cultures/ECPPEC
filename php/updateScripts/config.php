cc149a270c6c19238b3c9a246a3577e91bc670ac
Tom Schofield <tomschofieldart@gmail.com>
2021-03-05 14:52:51 +0000

    clean up better filters and fixed timeline slider

---
+++ php/updateScripts/config.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "127.0.0.1:8889";
+$username = "root";
+$password = "root";
+
+// $username = "tomschof_2000";
+// $password = "sxnu2727";
+//$dbname = "tomschof_ECCPEC";
+
+
+
+$dbname = "ECPPEC_TEST_1";
+
+?>
