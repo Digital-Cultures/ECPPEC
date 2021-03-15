9505313e0e74d2ead5a95dd5454d6519c245ffbe
John Schoneboom <john@bonkworld.org>
2021-03-15 09:19:58 +0000

    Simplify and secure elections API
    Did a bit of tidying up the getElections code to simplify the
    way the WHERE clause is constructed, and modified it so the query
    is run as a prepared statement for some added security.

--- php/getElections.php
+++ php/getElections.php
@@ -1,227 +1,97 @@
 <?php
 
 require_once('config.php');
-//?from_year=1734&to_year=1760
 
-require_once('connect.php');
-// $year_range ="";
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-$sql = "SELECT * FROM elections";
+//initialize some variables
+$sql = "SELECT * FROM elections ";
+$optimized = array(); //will enforce lowercase keys for $_GET array
+$options = array(); //for building WHERE clause, derived from optimized $_GET variables
+$big_array = array(); //will contain all user parameters in order, for use in prepared query
 
-$is_first_filter = true;
-
-if (isset($_GET["from_year"]) && isset($_GET["to_year"])  ){
-	$year_range = " where year > ".$_GET["from_year"]." and "."year < ".$_GET["to_year"];
-	//echo $year_range."<br>";
-	$sql .= $year_range;
-	$is_first_filter = false;
-}
-if ( isset($_GET["month"])  ){
-//	echo sizeof(explode(";", $_GET["month"] )  );
-
-	$month ="";
-
-	if(sizeof(explode(";", $_GET["month"] )  )>1){
-
-		if($is_first_filter){
-			$month = " where ( month = ";
-			$is_first_filter = false;
-		}
-		else{
-			$month = " and ( month = ";
-		}
-		
-		$month.="'".explode(";", $_GET["month"] )[0]."'";
-
-		foreach (explode(";", $_GET["month"] )  as $key => $value) {
-//			echo "looking for:. ".$value."<br>";
-			$month.=" or month = '".$value."'";
-
-		}
-		$month.=" ) ";
-	}
-	else{
-		if($is_first_filter){
-			$month = " where month = '".$_GET["month"]."'";
-			$is_first_filter = false;
-		}
-		else{
-			$month = " and month = '".$_GET["month"]."'";
-		}
-
-	}
-
-	
-	//echo $year_range."<br>";
-	$sql .= $month;
+//lowercase all keys, eliminate inconsistency headaches
+if(is_array($_GET)) {
+	$optimized = optimizer($_GET);
 }
 
-
-if ( isset($_GET["Constituency"])  ){
-//	echo sizeof(explode(";", $_GET["Constituency"] )  );
-
-	$Constituency ="";
-
-	if(sizeof(explode(";", $_GET["Constituency"] )  )>1){
-
-		if($is_first_filter){
-			$Constituency = " where ( Constituency = ";
-			$is_first_filter = false;
-		}
-		else{
-			$Constituency = " and ( Constituency = ";
-		}
-		
-		$Constituency.="'".explode(";", $_GET["Constituency"] )[0]."'";
-
-		foreach (explode(";", $_GET["Constituency"] )  as $key => $value) {
-		//	echo "looking for:. ".$value."<br>";
-			$Constituency.=" or Constituency = '".$value."'";
-
-		}
-		$Constituency.=" ) ";
-	}
-	else{
-		if($is_first_filter){
-			$Constituency = " where Constituency = '".$_GET["Constituency"]."'";
-			$is_first_filter = false;
-		}
-		else{
-			$Constituency = " and Constituency = '".$_GET["Constituency"]."'";
-		}
-
-	}
-
-	
-	//echo $year_range."<br>";
-	$sql .= $Constituency;
+//cast all years as integers for safety
+if (isset($optimized["from_year"]) && isset($optimized["to_year"])) {
+	$options[] = "election_year >= "
+	.	(int) $optimized["from_year"]
+	.	" AND election_year <= "
+	.	(int) $optimized["to_year"];
 }
 
-if ( isset($_GET["CountyBoroughUniv"])  ){
-//	echo sizeof(explode(";", $_GET["CountyBoroughUniv"] )  );
+//here we begin to set things up for a prepared statement
 
-	$CountyBoroughUniv ="";
-
-	if(sizeof(explode(";", $_GET["CountyBoroughUniv"] )  )>1){
-
-		if($is_first_filter){
-			$CountyBoroughUniv = " where ( CountyBoroughUniv = ";
-			$is_first_filter = false;
-		}
-		else{
-			$CountyBoroughUniv = " and ( CountyBoroughUniv = ";
-		}
-		
-		$CountyBoroughUniv.="'".explode(";", $_GET["CountyBoroughUniv"] )[0]."'";
-
-		foreach (explode(";", $_GET["CountyBoroughUniv"] )  as $key => $value) {
-	//		echo "looking for:. ".$value."<br>";
-			$CountyBoroughUniv.=" or CountyBoroughUniv = '".$value."'";
-
-		}
-		$CountyBoroughUniv.=" ) ";
-	}
-	else{
-		if($is_first_filter){
-			$CountyBoroughUniv = " where CountyBoroughUniv = '".$_GET["CountyBoroughUniv"]."'";
-			$is_first_filter = false;
-		}
-		else{
-			$CountyBoroughUniv = " and CountyBoroughUniv = '".$_GET["CountyBoroughUniv"]."'";
-		}
-
-	}
-
-	
-	//echo $year_range."<br>";
-	$sql .= $CountyBoroughUniv;
+if (isset($optimized["month"])) {
+	$array = explode(";",$optimized['month']);
+	$big_array = array_merge($big_array,$array);
+	$options[] = "election_month IN ("
+	.	str_repeat("?,",count($array)-1)
+	.	"?)";
 }
 
-if ( isset($_GET["ByElectionGeneral"])  ){
-//	echo sizeof(explode(";", $_GET["ByElectionGeneral"] )  );
-
-	$ByElectionGeneral ="";
-
-	if(sizeof(explode(";", $_GET["ByElectionGeneral"] )  )>1){
-
-		if($is_first_filter){
-			$ByElectionGeneral = " where ( ByElectionGeneral = ";
-			$is_first_filter = false;
-		}
-		else{
-			$ByElectionGeneral = " and ( ByElectionGeneral = ";
-		}
-		
-		$ByElectionGeneral.="'".explode(";", $_GET["ByElectionGeneral"] )[0]."'";
-
-		foreach (explode(";", $_GET["ByElectionGeneral"] )  as $key => $value) {
-			//echo "looking for:. ".$value."<br>";
-			$ByElectionGeneral.=" or ByElectionGeneral = '".$value."'";
-
-		}
-		$ByElectionGeneral.=" ) ";
-	}
-	else{
-		if($is_first_filter){
-			$ByElectionGeneral = " where ByElectionGeneral = '".$_GET["ByElectionGeneral"]."'";
-			$is_first_filter = false;
-		}
-		else{
-			$ByElectionGeneral = " and ByElectionGeneral = '".$_GET["ByElectionGeneral"]."'";
-		}
-
-	}
-
-	
-	//echo $year_range."<br>";
-	$sql .= $ByElectionGeneral;
+if (isset($optimized["constituency"])) {
+	$array = explode(";",$optimized['constituency']);
+	$big_array = array_merge($big_array,$array);
+	$options[] = "constituency IN ("
+	.	str_repeat("?,",count($array)-1)
+	.	"?)";
 }
-if ( isset($_GET["Contested"])  ){
-//	echo sizeof(explode(";", $_GET["Contested"] )  );
 
-	$Contested ="";
-
-	if(sizeof(explode(";", $_GET["Contested"] )  )>1){
-
-		if($is_first_filter){
-			$Contested = " where ( Contested = ";
-			$is_first_filter = false;
-		}
-		else{
-			$Contested = " and ( Contested = ";
-		}
-		
-		$Contested.="'".explode(";", $_GET["Contested"] )[0]."'";
-
-		foreach (explode(";", $_GET["Contested"] )  as $key => $value) {
-		//	echo "looking for:. ".$value."<br>";
-			$Contested.=" or Contested = '".$value."'";
-
-		}
-		$Contested.=" ) ";
-	}
-	else{
-		if($is_first_filter){
-			$Contested = " where Contested = '".$_GET["Contested"]."'";
-			$is_first_filter = false;
-		}
-		else{
-			$Contested = " and Contested = '".$_GET["Contested"]."'";
-		}
-
-	}
-
-	
-	//echo $year_range."<br>";
-	$sql .= $Contested;
+if (isset($optimized["countyboroughuniv"])) {
+	$array = explode(";",$optimized['countyboroughuniv']);
+	$big_array = array_merge($big_array,$array);
+	$options[] = "countyboroughuniv IN ("
+	.	str_repeat("?,",count($array)-1)
+	.	"?)";
 }
+
+if (isset($optimized["byelectiongeneral"])) {
+	$array = explode(";",$optimized['byelectiongeneral']);
+	$big_array = array_merge($big_array,$array);
+	$options[] = "by_election_general IN ("
+	.	str_repeat("?,",count($array)-1)
+	.	"?)";
+}
+
+if (isset($optimized["contested"])) {
+	$array = explode(";",$optimized['contested']);
+	$big_array = array_merge($big_array,$array);
+	$options[] = "contested IN ("
+	.	str_repeat("?,",count($array)-1)
+	.	"?)";
+	}
+
+if(count($options)) {
+	$sql .= "WHERE "
+	.	implode(" AND ",$options);
+}
+
+$stmt  = $conn->prepare($sql); // prepare
+$types = str_repeat('s', count($big_array)); //types
+$stmt->bind_param($types, ...$big_array); // bind array at once
+$stmt->execute();
+$result = $stmt->get_result(); // get the mysqli result
+$rows = $result->fetch_all(MYSQLI_ASSOC); // fetch the data
+
+$response = array(
+	"num_results"=>sizeof($rows),
+	"earliest_year"=>getEarliestYear($rows),
+	"latest_year"=>getLatestYear($rows),
+	"elections"=>$rows
+);
+print json_encode($response);
+$conn->close();
 
 function getEarliestYear($rows){
 	$earliest_year = 3000;
 	foreach ($rows as $key => $value) {
 		//echo $value['Year']."<br>";
-		if($value['Year']<$earliest_year){
-			$earliest_year=$value['Year']; 
+		if($value['election_year'] < $earliest_year){
+			$earliest_year = $value['election_year'];
 
 		}
 	}
@@ -231,31 +101,26 @@ function getLatestYear($rows){
 	$latest_year = 0;
 	foreach ($rows as $key => $value) {
 		//echo $value['Year']."<br>";
-		if($value['Year']>$latest_year){
-			$latest_year=$value['Year']; 
+		if($value['election_year'] > $latest_year){
+			$latest_year=$value['election_year'];
 
 		}
 	}
 	return $latest_year;
 }
 
-// echo $sql;
-$result = $conn->query($sql);
-$rows = array();
-while($r = mysqli_fetch_assoc($result)) {
-	$rows[] = $r;
-	   // echo $r;
+/**
+ * 
+ * @param array $array
+ * @return same array with keys in all lowercase, as failsafe against user inconsistency
+ */
+function optimizer($array) {
+	$optimized = array();
+	foreach($array as $k => $v) {
+		$optimized[strtolower($k)] = $v;
+	}
+	return $optimized;
 }
-
-$response = array(
-	"num_results"=>sizeof($rows),
-	"earliest_year"=>getEarliestYear($rows),
-	"latest_year"=>getLatestYear($rows),
-	"elections"=>$rows
-);
-//print_r($result);
- print json_encode($response);
-$conn->close();
 
 
 ?>
